f2eb9b7cde2f7a1b0d741eefcadbc68206607bb5
Minh Tran <s0540128@htw-berlin.de>
2018-05-04 07:34:12 +0200

    routing updated
    remove routing prefix (!) from url
    set to html5 history mode (rmoved hashtag from routing)

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,29 +9,35 @@
  * Main module of the application.
  */
 angular
-  .module('moveditorApp', [
-    'ngAnimate',
-    'ngAria',
-    'ngCookies',
-    'ngMessages',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+    .module('moveditorApp', [
+        'ngAnimate',
+        'ngAria',
+        'ngCookies',
+        'ngMessages',
+        'ngResource',
+        'ngRoute',
+        'ngSanitize',
+        'ngTouch'
+    ])
+    .config(['$locationProvider', function($locationProvider) {
+        $locationProvider.hashPrefix('');
+    }])
+    .config(function ($routeProvider, $locationProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl',
+                controllerAs: 'main'
+            })
+            .when('/about', {
+                templateUrl: 'views/about.html',
+                controller: 'AboutCtrl',
+                controllerAs: 'about'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+
+        // use the HTML5 History API
+        $locationProvider.html5Mode(true);
+    });

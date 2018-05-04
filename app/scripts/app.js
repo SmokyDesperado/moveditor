89caefcd2125536fb8000c258e73e0c2fb9c3c2a
Minh Tran <s0540128@htw-berlin.de>
2018-05-04 07:29:15 +0200

    angularjs added
    yeoman generator used to create angularjs base project

---
+++ app/scripts/app.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name moveditorApp
+ * @description
+ * # moveditorApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('moveditorApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });

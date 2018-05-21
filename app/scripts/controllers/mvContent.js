9309b6613d4c09edb760957e6616929678dcfa80
Smoky Desperado <desperadosmoky@gmail.com>
2018-05-21 15:23:13 +0200

    content area updated
    content directive added
    some styling updated

---
+++ app/scripts/controllers/mvContent.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name moveditorApp.controller:MvContentCtrl
+ * @description
+ * # MvContentCtrl
+ * Controller of the moveditorApp
+ */
+angular.module('moveditorApp')
+    .controller('MvContentCtrl', [
+        function () {
+            this.params = {
+                'test': 'test 1'
+            };
+
+            this.test = 'test 2';
+
+            this.doStuff = function () {
+                console.log('do stuff');
+            };
+
+            this.hasStuff = function (stuff) {
+                if(stuff) {
+                    return true;
+                }
+
+                return false;
+            }
+        }
+    ]);
---
+++ app/scripts/directives/mvContent.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name moveditorApp.directive:navigator
+ * @description
+ * # navigator
+ */
+angular.module('moveditorApp')
+    .directive('mvContent', [function (mvNavService) {
+        return {
+            templateUrl: '/views/directives/mv_content.html',
+            replace: true,
+            restrict: 'AE',
+            controller: 'MvContentCtrl',
+            bindToController: true,
+            controllerAs:'contentCtrl',
+            link: function($scope, $element, $attrs, contentCtrl) {
+                $scope.click = function(param) {
+                    console.log('clicked with param:', param);
+                };
+
+                $scope.doStuff = function() {
+                    contentCtrl.doStuff();
+                }
+            }
+        };
+    }]);

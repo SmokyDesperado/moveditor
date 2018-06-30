98335ecfe0bcd9577480839b2ab7619dde144e71
Smoky Desperado <desperadosmoky@gmail.com>
2018-06-30 08:53:09 +0200

    timeline updated
    drag and drop prototyped dummy

--- app/scripts/directives/mvTimeline.js
+++ app/scripts/directives/mvTimeline.js
@@ -19,12 +19,54 @@ angular.module('moveditorApp')
 
                 $scope.timelineService = TimelineService;
 
+                var self = this;
+                this.dragClone = null;
+
                 $scope.click = function (param) {
                     console.log('clicked with param:', param);
                 };
 
                 $scope.tap = function($event) {
                     TimelineCtrl.tap($event);
+                };
+
+                $scope.panStart = function (timelineObjectKey) {
+                    if(TimelineCtrl.focus === timelineObjectKey) {
+                        self.dragClone = $scope.timelineService.timelineList[timelineObjectKey];
+                        console.log('target: ', $scope.timelineService.timelineList[timelineObjectKey]);
+                    }
+
+                    // self.dragClone = angular.copy($event.target);
+                    // angular.element(self.dragClone).addClass('drag--clone');
+                    // $event.element[0].parentElement.parentElement.parentElement.prepend(self.dragClone);
+                    // self.dragClone.style['position'] = 'absolute';
+                    //
+                    // DragAndDropService.panMoveStarted($scope.contentObjectKey);
+                };
+
+                $scope.hammerPanMove = function ($event) {
+                    if(self.dragClone) {
+                        console.log('target: ', $event.center);
+                    }
+
+                    // var x = $event.center.x - $event.target.offsetWidth / 2,
+                    //     y = $event.center.y - $event.target.offsetHeight;
+                    //
+                    // self.dragClone.style['left'] = x + 'px';
+                    // self.dragClone.style['top'] = y + 'px';
+                    //
+                    // DragAndDropService.panMove($event);
+                };
+
+                $scope.panEnd = function ($event) {
+                    if(self.dragClone) {
+                        console.log('pam end', $event.center);
+                        self.dragClone = null;
+                    }
+                    // angular.element(self.dragClone).remove();
+                    // self.dragClone = null;
+                    //
+                    // DragAndDropService.panMoveEnd($event, $scope.contentObjectKey);
                 };
 
                 TimelineCtrl.initTimelineElement($element.find('#timelineDropArea'));

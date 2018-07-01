c90a057b0872c8a0d945aedd24cbd5ea7fce68ed
Smoky Desperado <desperadosmoky@gmail.com>
2018-07-01 15:02:53 +0200

    timeline updated
    drag timeline object limit start and end is end of previous object and
    start of next object

--- app/scripts/directives/mvTimeline.js
+++ app/scripts/directives/mvTimeline.js
@@ -7,7 +7,7 @@
  * # navigator
  */
 angular.module('moveditorApp')
-    .directive('mvTimeline', ['TimelineService', '$document', function (TimelineService, $document) {
+    .directive('mvTimeline', ['TimelineService', '$document', 'ContentService', function (TimelineService, $document, ContentService) {
         return {
             templateUrl: '/views/directives/mvTimeline.html',
             replace: true,
@@ -22,6 +22,8 @@ angular.module('moveditorApp')
                 var self = this;
                 this.dragClone = null;
                 this.dragOffset = 0;
+                this.dragFreeSpaceStart = 0;
+                this.dragFreeSpaceEnd = 1920;
 
                 $scope.click = function (param) {
                     console.log('clicked with param:', param);
@@ -33,35 +35,61 @@ angular.module('moveditorApp')
 
                 $scope.panStart = function ($event, timelineObjectKey) {
                     if(TimelineCtrl.focus === timelineObjectKey) {
-                        var chunk = angular.element($event.target);
-                        self.dragOffset = $event.center.x - chunk[0].offsetLeft;
+                        self.initDragLimitValues($event, timelineObjectKey);
                     }
                 };
 
-                $scope.hammerPanMove = function ($event) {
-                    if($event.center.x - self.dragOffset >= 0) {
-                        var chunk = angular.element($event.target);
-                        chunk[0].style['left'] = ($event.center.x - self.dragOffset) + 'px';
+                $scope.hammerPanMove = function ($event, timelineObjectKey) {
+                    var timelineObjectLength = ($scope.timelineService.timelineList[timelineObjectKey].end - $scope.timelineService.timelineList[timelineObjectKey].start) *
+                        $scope.timelineService.pixelPerSeconds;
+                    if($event.center.x - self.dragOffset >= self.dragFreeSpaceStart &&
+                        ($event.center.x - self.dragOffset + timelineObjectLength) <= self.dragFreeSpaceEnd) {
+                        self.dragTimelineObject($event, timelineObjectKey);
                     }
                 };
 
                 $scope.panEnd = function ($event) {
-                    if(self.dragClone) {
-                        // console.log('pam end', $event.center);
-                        angular.element(self.dragClone).remove();
-                        self.dragClone = null;
-                        self.dragOffset = 0;
+                    self.dragOffset = 0;
+                };
+
+                this.dragTimelineObject = function($event, timelineObjectKey) {
+                    var chunk = angular.element($event.target);
+                    chunk[0].style['left'] = ($event.center.x - self.dragOffset) + 'px';
+
+                    $scope.timelineService.timelineList[timelineObjectKey].start = self.quantizeDraggedTime(
+                        ($event.center.x - self.dragOffset) / $scope.timelineService.pixelPerSeconds);
+                    $scope.timelineService.timelineList[timelineObjectKey].end = self.quantizeDraggedTime(
+                        $scope.timelineService.timelineList[timelineObjectKey].start + ContentService.contentList[$scope.timelineService.timelineList[timelineObjectKey].objectListId].length);
+                };
+
+                this.initDragLimitValues = function ($event, timelineObjectKey) {
+                    self.dragOffset = $event.center.x - angular.element($event.target)[0].offsetLeft;
+
+                    if(angular.isDefined($scope.timelineService.timelineList[timelineObjectKey - 1])) {
+                        self.dragFreeSpaceStart = $scope.timelineService.timelineList[timelineObjectKey - 1].end * $scope.timelineService.pixelPerSeconds;
+                    }
+                    else {
+                        self.dragFreeSpaceStart = 0;
+                    }
+
+                    if(angular.isDefined($scope.timelineService.timelineList[timelineObjectKey + 1])) {
+                        self.dragFreeSpaceEnd = $scope.timelineService.timelineList[timelineObjectKey + 1].start * $scope.timelineService.pixelPerSeconds;
+                    }
+                    else {
+                        self.dragFreeSpaceEnd = $scope.timelineService.timelineWidth;
                     }
                 };
 
-                this.createDragCloneElement = function() {
-                    var dragCloneElement = angular.element(
-                        '<div class="timeline-chunk--clone"></div>'
-                    );
-                    var body = $document.find('body').eq(0);
+                this.quantizeDraggedTime = function (timevalue) {
+                    return Math.round(timevalue * $scope.timelineService.timelineQuantization) / $scope.timelineService.timelineQuantization;
+                };
 
-                    body.prepend(dragCloneElement);
-                    self.dragClone = dragCloneElement[0];
+                this.dragTimelineWidthGrow = function() {
+
+                };
+
+                this.dragTimelineScroll = function() {
+
                 };
 
                 TimelineCtrl.initTimelineElement($element.find('#timelineDropArea'));
